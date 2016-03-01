e773037636c0f90a447db3af3a96c1225bfc22db
、Campanulaceae <932191671@qq.com>
2016-03-01 22:24:16 +0800

    MarioDb & Test

---
+++ src/test/java/com/junicorn/mario/db/MarioDbTest.java
@@ -0,0 +1,67 @@
+package com.junicorn.mario.db;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class MarioDbTest {
+	public static void main(String[] args) {
+		try {
+			// 解决tomcat下报错
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		MarioDb.init("jdbc:mysql://127.0.0.1:3306/mario_sample", "root", "root");
+
+//		testGet();
+//		testGet2();
+		// testGetList();
+		// testGetList2();
+		testGetMap();
+	}
+
+	//  带参get测试
+	public static void testGet() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "rose");
+		map.put("id", 3);
+		User user = MarioDb.get("select * from t_user where name = :name && id = :id", User.class, map);
+		System.out.println(user);
+	}
+
+	// 无参get测试
+	public static void testGet2() {
+		User user = MarioDb.get("select * from t_user", User.class);
+		System.out.println(user);
+	}
+
+	// 无参getList测试
+	public static void testGetList() {
+		List<User> list = MarioDb.getList("select * from t_user", User.class);
+
+		for (User u : list) {
+			System.out.println(u);
+		}
+	}
+
+	// 带参getList带参
+	public static void testGetList2() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "rose");
+		List<User> list = MarioDb.getList("select * from t_user where name = :name", User.class, map);
+
+		for (User u : list) {
+			System.out.println(u);
+		}
+	}
+	
+	public static void testGetMap() {
+		Map<String, Object> map = MarioDb.getMap("select * from t_user");
+		
+		System.out.println("=====");
+	}
+}
